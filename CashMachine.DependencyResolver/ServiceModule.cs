479933ef270f46ec7740f5b57cc7625001d5f13c
tmatharu <tejindermatharu@hotmail.com>
2016-08-14 22:19:07 +0100

    including service module

---
+++ CashMachine.DependencyResolver/ServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+using CashMachine.Interfaces;
+using CashMachine.Repository;
+using CashMachine.Services;
+
+namespace CashMachine.DependencyResolver
+{
+
+    public class ServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+            //Since we are not using a persistence store for the repository, we'll use singletonscope to maintain the state of the repository
+            Bind<ICashRepository>().To<CashRepository>().InSingletonScope();
+        }
+    }
+}
+
